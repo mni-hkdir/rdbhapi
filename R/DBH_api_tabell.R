90009f81ce1588da4b175e2e28dcb5472cac010d
sigve-berge-hofland <hof@kd.dep.no>
2019-11-21 13:14:22 +0100

    Bruker `utils::download.file` for bulknedlasting, i stedet for `readr::read_delim` direkte på url

--- R/DBH_api_tabell.R
+++ R/DBH_api_tabell.R
@@ -32,38 +32,39 @@
     for (i in seq_along(filters)) {
       filter_query[[i]] <-
         list(variabel = enc2utf8(names(filters)[i]),
-          selection = list(filter = "item",
-            values = lapply(filters[[i]],
-              function(s) enc2utf8(as.character(s)))))
+             selection = list(filter = "item",
+                              values = lapply(filters[[i]],
+                                              function(s) enc2utf8(as.character(s)))))
       if (filters[[i]][1] == "*") {
         filter_query[[i]]$selection$filter <- "all"
         if (names(filters)[i] %in% names(exclude)) {
-          filter_query[[i]]$selection$exclude = lapply(exclude[[names(filters)[i]]],
-            function(s) enc2utf8(as.character(s)))
+          filter_query[[i]]$selection$exclude <-
+            lapply(exclude[[names(filters)[i]]],
+                   function(s) enc2utf8(as.character(s)))
         }
       } else if (filters[[i]][1] %in% c("top",  "lessthan", "greaterhan")) {
         filter_query[[i]]$selection$filter <- filters[[i]][1]
-        filter_query[[i]]$selection$values <- lapply(filters[[i]][2],
-          function(s) enc2utf8(as.character(s)))
+        filter_query[[i]]$selection$values <-
+          lapply(filters[[i]][2], function(s) enc2utf8(as.character(s)))
         if (names(filters)[i] %in% names(exclude)) {
-          filter_query[[i]]$selection$exclude = lapply(exclude[[names(filters)[i]]],
-            function(s) enc2utf8(as.character(s)))
+          filter_query[[i]]$selection$exclude <-
+            lapply(exclude[[names(filters)[i]]], function(s) enc2utf8(as.character(s)))
         }
       } else if (filters[[i]][1] == "between") {
         filter_query[[i]]$selection$filter <- "between"
-        filter_query[[i]]$selection$values <- c(lapply(filters[[i]][2],
-          function(s) enc2utf8(as.character(s))),lapply(filters[[i]][3],
-            function(s) enc2utf8(as.character(s))))
+        filter_query[[i]]$selection$values <-
+          c(lapply(filters[[i]][2], function(s) enc2utf8(as.character(s))),
+            lapply(filters[[i]][3], function(s) enc2utf8(as.character(s))))
         if (names(filters)[i] %in% names(exclude)) {
-          filter_query[[i]]$selection$exclude = lapply(exclude[[names(filters)[i]]],
-            function(s) enc2utf8(as.character(s)))
+          filter_query[[i]]$selection$exclude <-
+            lapply(exclude[[names(filters)[i]]], function(s) enc2utf8(as.character(s)))
         }
       } else if (names(filters)[i] %in% names(exclude)) {
         exclude_warning <-
           paste0(ifelse(length(exclude_warning) == 0,
-            "Excluding filters cannot be combined with the item filter for the following variables: ",
-            paste0(exclude_warning, ", ")),
-            names(filters)[i])
+                        "Excluding filters cannot be combined with the item filter for the following variables: ",
+                        paste0(exclude_warning, ", ")),
+                 names(filters)[i])
       }
     }
 
@@ -112,9 +113,13 @@ dbh_data <- function(
   api_version = 1) {
   if (is.null(filters)) {
     url <- paste0("https://api.nsd.no/dbhapitjener/Tabeller/bulk-csv?rptNr=", table_id)
-    res <- httr::GET(url,
-                     httr::add_headers(Authorization =
-                                         paste("Bearer", .get_token(), sep = " ")))
+    temp_file <- tempfile()
+    on.exit(unlink(temp_file))
+    download.file(url,
+                  destfile = temp_file,
+                  headers = c(Authorization =
+                                paste("Bearer", .get_token(), sep = " ")))
+    res <- temp_file
     delim_csv <- ","
   } else {
     query <- .make_query(table_id = table_id, filters = filters, group_by = group_by, sort_by = sort_by, exclude = exclude)
@@ -131,27 +136,28 @@ dbh_data <- function(
                  body = post_body,
                  encode = 'json')
     delim_csv <- ";"
-  }
-  res_text_content <- httr::content(res, "text")
-  if (httr::http_error(res)) {
-    res_parsed <-
-      jsonlite::fromJSON(res_text_content,
-                         simplifyVector = FALSE)
-    stop(sprintf("DBH-API request failed\n%s\n%s: %s",
-                 res$url,
-                 httr::http_status(res)$message,
-                 res_parsed$message),
-         call. = FALSE)
-  }
-  res_type <- httr::http_type(res)
-  if (!identical(res_type, "text/csv")) {
-    stop(sprintf("DBH-API request returned type '%s' and not 'text/csv' as expected",
-                 res_type),
-         call. = FALSE)
+    res_text_content <- httr::content(res, "text")
+    if (httr::http_error(res)) {
+      res_parsed <-
+        jsonlite::fromJSON(res_text_content,
+                           simplifyVector = FALSE)
+      stop(sprintf("DBH-API request failed\n%s\n%s: %s",
+                   res$url,
+                   httr::http_status(res)$message,
+                   res_parsed$message),
+           call. = FALSE)
+    }
+    res_type <- httr::http_type(res)
+    if (!identical(res_type, "text/csv")) {
+      stop(sprintf("DBH-API request returned type '%s' and not 'text/csv' as expected",
+                   res_type),
+           call. = FALSE)
+    }
+    res <- res_text_content
   }
   data <-
     readr::read_delim(
-      res_text_content,
+      res,
       delim = delim_csv,
       col_types = readr::cols(.default = readr::col_character()),
       locale = readr::locale(decimal_mark = "."),
