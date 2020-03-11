b036fa68a719231c247aed122b9404d7810fa44c
makinin <57001187+makinin@users.noreply.github.com>
2020-03-11 21:59:28 +0100

    Update test_rdbhapi.R

--- tests/testthat/test_rdbhapi.R
+++ tests/testthat/test_rdbhapi.R
@@ -2,9 +2,4 @@ context("token test")
 test_that("dbh_data returns data when data set exists in set of bulk data",
   {expect_equal(dbh_data(211)[[1]][1], "0211")})
 
-test_that("dbh_data with fileter values",
-  {expect_equal(dbh_data(211, filters = list("Kortnavn" = "UIB"))[[1]][1], "1120")})
-
-test_that("dbh_data returns error in case of agregate tables without defined group_by",
-  {expect_error(dbh_data(142, filters = list("Årstall" = c("top","5"))))})
 
