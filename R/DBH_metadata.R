4bf26a23f5a8e814bb6300204ec312285f6c49b7
sigve-berge-hofland <hof@kd.dep.no>
2020-03-31 21:05:09 +0200

    Lagrer metadata og innholdsfortegnelse i minnet så vi slipper å laste ned disse tabellene så ofte.

--- R/DBH_metadata.R
+++ R/DBH_metadata.R
@@ -2,87 +2,119 @@
 #'
 #' @description Gets information on variables included in DBH datasets (type of variable, data type).
 #'
-#' @param table_id A vector of code names for the datasets to get variable information for
-#' @importFrom httr GET
-#' @importFrom httr content
-#' @importFrom readr read_delim
-#' @importFrom readr col_character
-#' @importFrom readr locale
-#' @importFrom readr show_progress
-#' @importFrom dplyr %>%
-#' @importFrom tibble as_tibble
+#' @param table_id A vector of code names for the datasets to get variable information for, or NULL to get info for all variables.
 #' @return A tibble
 #' @export
 #' @examples
 #' dbh_metadata(88)
 
-
-
-
-dbh_metadata <- function(table_id){
-  url_meta <- "https://api.nsd.no/dbhapitjener/Tabeller/bulk-csv?rptNr=002"
-  res <- httr::GET(url_meta)
-  res <- httr::content(res, as = "text")
-  metadata <-
-    readr::read_delim(
-      res,
-      delim = ",",
-      col_types = readr::cols(.default = readr::col_character()),
-      locale = readr::locale(decimal_mark = "."),
-      na = "",
-      trim_ws = TRUE,
-      progress = readr::show_progress()
-    )
-  metadata[as.integer(metadata[["Tabell id"]]) %in% as.integer(table_id), ]
+dbh_metadata <- function(table_id = NULL) {
+  metadata <- .get_new_metadata()
+  if (!is.null(table_id)) {
+    metadata <- metadata[metadata[["Tabell id"]] %in% as.character(table_id), ]
+  }
+  metadata
 }
 
-#' @title Get content data from DBH-API
-#'
-#' @param table_id A vector of code names for the datasets to get variable information for
-#'
-#' @return A tibble
+#' Download table with metadata for variables in DBH-API
 #' @keywords internal
 
-.dbh_content<-function(table_id){
-  url_content<-"https://api.nsd.no/dbhapitjener/Tabeller/bulk-csv?rptNr=001"
-  res <- httr::GET(url_content)
-  res <- httr::content(res, as = "text")
-  content <-
+.get_new_metadata <- function() {
+  url_meta <- "https://api.nsd.no/dbhapitjener/Tabeller/bulk-csv?rptNr=002"
+  res <- httr::GET(url_meta)
+  res <- httr::content(res, as = "text", encoding = "UTF-8")
+  res <-
     readr::read_delim(
       res,
       delim = ",",
       col_types = readr::cols(.default = readr::col_character()),
-      locale = readr::locale(decimal_mark = "."),
       na = "",
       trim_ws = TRUE,
-      progress = readr::show_progress()
+      progress = FALSE
     )
-  content[as.integer(content[["Tabell id"]]) %in% as.integer(table_id), ]
+  res
 }
 
-#' Title
+
+#' Get table with metadata for variables in the DBH API
+#' @keywords internal
+
+.get_metadata <- function(table_id = NULL) {
+  t <- Sys.time()
+  if (t >= .env$metadata_expiration) {
+    .env$metadata <- .get_new_metadata()
+    .env$metadata_expiration <- t + 86400
+  }
+  metadata <- .env$metadata
+  if (!is.null(table_id)) {
+    metadata <- metadata[metadata[["Tabell id"]] %in% as.character(table_id), ]
+  }
+  metadata
+}
+
+
+#' @title Table of contents for DBH-API
+#'
+#' @param table_id A vector of code names for the datasets retrieve information for, or NULL to get the whole table of contents
+#'
+#' @return A tibble
+
+dbh_toc <- function(table_id = NULL) {
+  toc <- .get_new_toc()
+  if (!is.null(table_id)) {
+    toc <- toc[toc[["Tabell id"]] %in% as.character(table_id), ]
+  }
+  toc
+}
+
+
+#' Download table of contents of the DBH API
+#' @keywords internal
+
+.get_new_toc <- function() {
+  url_meta <- "https://api.nsd.no/dbhapitjener/Tabeller/bulk-csv?rptNr=001"
+  res <- httr::GET(url_meta)
+  res <- httr::content(res, as = "text", encoding = "UTF-8")
+  res <-
+    readr::read_delim(
+      res,
+      delim = ",",
+      col_types = readr::cols(.default = readr::col_character()),
+      na = "",
+      trim_ws = TRUE,
+      progress = FALSE
+    )
+  res
+}
+
+#' Get table with content for variables in the DBH-API
+#' @keywords internal
+
+.get_toc <- function(table_id = NULL) {
+  t <- Sys.time()
+  if (t >= .env$toc_expiration) {
+    .env$toc <- .get_new_toc()
+    .env$toc_expiration <- t + 86400
+  }
+  toc <- .env$toc
+  if (!is.null(table_id)) {
+    toc <- toc[toc[["Tabell id"]] %in% as.character(table_id), ]
+  }
+  toc
+}
+
+#' Default group_by for a table as suggested by DBH
 #'
 #' @param table_id A vector of code names for the datasets to get variable information for
-
-#' @importFrom dplyr filter
-#' @importFrom rlang is_empty
 #' @return A list of group by variables
+#' @keywords internal
 
-#'
-
-.dbh_groupBy <- function(table_id){
-  metadata <- dbh_metadata(table_id)
-  group_by <- as_tibble(metadata  %>% dplyr::filter(metadata[["Group by (forslag)"]] =="J"))
-
-
-  if (rlang::is_empty(group_by[["Group by (forslag)"]]))
-  {
-    group_by=NULL
-  }
-  else
-  {
-    group_by=as.list(group_by[["Variabel navn"]])
-  }
+.default_group_by <- function(table_id){
+  metadata <- .get_metadata()
+  metadata <- metadata[(metadata$`Tabell id` %in% table_id) &
+                         (metadata$`Group by (forslag)` %in% "J"), ]
+  group_by <- metadata[["Variabel navn"]]
+  group_by <- if (length(group_by) == 0) NULL else as.list(group_by)
   group_by
 }
 
